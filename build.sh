83d2563a45eb7071b6ce4518fc75a0a3bd294d6a
Simon Egli <simon.egli@u-blox.com>
2018-08-16 09:13:30 +0200

    Make sure the required directories are created up on building the container

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build -t deadolus/android-studio .
+mkdir -p studio-data/profile/.AndroidStudio2.3 || exit
+mkdir -p studio-data/profile/.android || exit
+mkdir -p studio-data/profile/.java || exit
+mkdir -p studio-data/profile/.gradle || exit
+docker build -t u-blox/android-studio . || exit
