2c700862f309783a2f78fa355b65e3a63a6599c2
Simon Egli <git@egli.online>
2020-02-19 08:22:48 +0100

    Removing u-blox name with my handle
    Signed-off-by: Simon Egli <git@egli.online>

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ mkdir -p studio-data/profile/.AndroidStudio2.3 || exit
 mkdir -p studio-data/profile/.android || exit
 mkdir -p studio-data/profile/.java || exit
 mkdir -p studio-data/profile/.gradle || exit
-docker build -t u-blox/android-studio . || exit
+docker build -t deadolus/android-studio . || exit
