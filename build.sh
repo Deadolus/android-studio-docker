166fdf39358c2f842750614ac89a0c63b9fa6ab5
Simon Egli <git@egli.online>
2020-02-19 09:17:48 +0100

    Add new Android directory

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir -p studio-data/profile/.AndroidStudio3.5 || exit
+mkdir -p studio-data/Android || exit
 mkdir -p studio-data/profile/.android || exit
 mkdir -p studio-data/profile/.java || exit
 mkdir -p studio-data/profile/.gradle || exit
